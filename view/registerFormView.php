fdbfe2493ae07c0b82c661fae6bea553218a6555
damienn7 <damien.nicolleau@epitech.eu>
2023-02-07 19:28:49 +0100

    ajout de la version de l'inscription ameliore de la partie formulaire

--- view/registerFormView.php
+++ view/registerFormView.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,12 @@
     <title>My meetic</title>
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
+
 <body>
     <header class="header-container">
         <nav class="header-container_nav-container">
-            <img src="https://iliusstu-a.akamaihd.net/www.meetic.fr/hpv/positive-75570a615a9.svg" class="header-container_nav-container_logo" alt="meetic logo">
+            <img src="https://iliusstu-a.akamaihd.net/www.meetic.fr/hpv/positive-75570a615a9.svg"
+                class="header-container_nav-container_logo" alt="meetic logo">
             <ul class="header-container_nav-container_ul">
                 <li class="header-container_nav-container_ul_li">Soirees celibataires</li>
                 <li class="header-container_nav-container_ul_li">Rencontres celibataires en France</li>
@@ -24,8 +27,28 @@
     </header>
     <main class="main-container">
         <div class="main-container_div-logo">
-            <img src="https://iliusstu-a.akamaihd.net/www.meetic.fr/hpv/pinkwhite-44d5e901294.svg" width="290" height="90" class="main-container_div-logo_img" alt="logo meetic">
+            <img src="https://iliusstu-a.akamaihd.net/www.meetic.fr/hpv/pinkwhite-44d5e901294.svg" width="290"
+                height="90" class="main-container_div-logo_img" alt="logo meetic">
+            <h1 class="main-container_div-logo_h1-slogan">Serious Dating, Crazy Love*</h1>
         </div>
+        <div class="main-container_div-bot">
+            <img src="https://iliusstu-a.akamaihd.net/www.meetic.fr/hpv/conversational-bot-9d221733c14.png" class="main-container_div-bot_img" alt="bot picture">
+        </div>
+        <form action="" method="post" class="main-container_form">
+            <div class="main-container_form_div-first-part">
+                <button class="btn btn--green-filled" name="homme" value="homme">
+                    Je recherche un homme
+                </button>
+                <button class="btn btn--green-filled" name="femme" value="autre">
+                    Je recherche une femme
+                </button>
+                <button class="btn btn--green-filled" name="autre" value="autre">
+                    Tout le monde
+                </button>
+
+            </div>
+        </form>
     </main>
 </body>
+
 </html>
